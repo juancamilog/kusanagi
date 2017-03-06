79061a8f4e398c26b4ddad75b8a920efeefcd355
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-03-06 14:47:36 -0500

    Added NN policies, removed the copy of PILOC under thirdparty. This will be the last commit in mercurial before migrating to git.

--- thirdparty/pilcoV0.9/util/pyrand.m
+++
@@ -1,8 +0,0 @@
-function R = pyrand(varargin)
-    if nargin > 1
-        dims = cellfun(@int32,varargin);
-    else
-        dims = varargin{1};
-    end
-    R = cellfun(@double,cell( py.numpy.random.rand(prod(dims)).tolist()));
-    R = reshape(R,dims);
